4b0c33f75ae8b1b6ed6f65345670751613f67a13
amyruth <anrutherford@gmail.com>
2018-08-29 02:51:58 -0400

    started offline caching but needs work

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: 'pk.eyJ1IjoiYW5ydXRoZXJmb3JkIiwiYSI6ImNqbDVoZGx5czBoa3Aza241YTYzaGRkeXEifQ.6oVVj2Z0fQyJcA6tp-Ra7A',
+        mapboxToken: 'TOKEN HERE',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
